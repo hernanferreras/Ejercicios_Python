10c50610b7a53a1b2f67706b5c9b129ee7393da4
Hernan Ferreras <hernanferreras@icloud.com>
2026-02-15 22:54:49 -0300

    Fix comment formatting in ladrillos.py

--- ladrillos.py
+++ ladrillos.py
@@ -1,4 +1,5 @@
-'''3.2.14   LAB   Fundamentos del bucle while
+'''
+3.2.14   LAB   Fundamentos del bucle while
 Escenario
 Escucha esta historia: Un niño y su padre, un programador de computadoras, juegan con bloques de madera. Están construyendo una pirámide.
 Su pirámide es un poco rara, ya que en realidad es una pared en forma de pirámide - es plana. La pirámide se apila de acuerdo con un principio simple: 
