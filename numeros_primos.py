2d04bb05d7000bb2302fa6f2275404b32aef00cf
Hernan Ferreras <hernanferreras@icloud.com>
2026-02-21 16:50:35 -0300

    Add is_prime function to verify prime numbers
    Implement is_prime function to check for prime numbers.

---
+++ numeros_primos.py
@@ -0,0 +1,34 @@
+'''
+Un número natural es primo si es mayor que 1 y no tiene divisores más que 1 y si mismo.
+¿Complicado? De ningúna manera. Por ejemplo, 8 no es un número primo, ya que puedes dividirlo entre 2 y 4 
+(no podemos usar divisores iguales a 1 y 8, ya que la definición lo prohíbe).
+Por otra parte, 7 es un número primo, ya que no podemos encontrar ningún divisor para el.
+Tu tarea es escribir una función que verifique si un número es primo o no.
+La función: se llama is_prime;
+toma un argumento (el valor a verificar)
+devuelve True si el argumento es un número primo, y False de lo contrario.
+Sugerencia: intenta dividir el argumento por todos los valores posteriores (comenzando desde 2) y verifica el resto 
+- si es cero, tu número no puede ser un número primo; analiza cuidadosamente cuándo deberías detener el proceso.
+Si necesitas conocer la raíz cuadrada de cualquier valor, puedes utilizar el operador **. Recuerda: la raíz cuadrada de x es lo mismo que x0.5.
+Complementa el código en el editor.
+Ejecuta tu código y verifica si tu salida es la misma que la nuestra.
+Salida esperada:
+Output
+2 3 5 7 11 13 17 19
+'''
+def is_prime(num):
+    #
+    # Escribe tu código aquí.
+    div = 2
+    while div < num:
+        if num % div == 0:
+            return False
+        else:
+            div += 1
+    return True
+    #
+
+for i in range(1, 20):
+    if is_prime(i + 1):
+        print(i + 1, end=" ")
+print()
