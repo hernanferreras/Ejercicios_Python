8cdc121c30e26a54f2df4ab35b22ed76fcf1ef3f
Hernan Ferreras <hernanferreras@icloud.com>
2026-02-23 09:14:31 -0300

    Fix formatting in bisiesto.py docstring

--- bisiesto.py
+++ bisiesto.py
@@ -1,5 +1,6 @@
 '''
-Tu tarea es escribir y probar una función que toma un argumento (un año) y devuelve True si el año es un año bisiesto, o False si no lo es.
+Tu tarea es escribir y probar una función que toma un argumento (un año) y devuelve True si el año es un año bisiesto, 
+o False si no lo es.
 La semilla de la función ya se muestra en el código esqueleto del editor.
 Nota: también hemos preparado un breve código de prueba, que puedes utilizar para probar tu función.
 El código utiliza dos listas - una con los datos de prueba y la otra con los resultados esperados. El código te dirá si alguno de tus resultados no es válido.
