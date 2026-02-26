033885c0ea9bebe9da215f3d8947e850d1bf2577
Hernan Ferreras <hernanferreras@icloud.com>
2026-02-26 13:24:05 -0300

    Implement conversion from miles per gallon to liters per 100km

--- conversion_combustible.py
+++ conversion_combustible.py
@@ -29,6 +29,7 @@ def liters_100km_to_miles_gallon(liters):
 def miles_gallon_to_liters_100km(miles):
     #
     # Escribe tu código aquí.
+    return((3.785411784*100)/(miles*1.609344))
     #
 
 print(liters_100km_to_miles_gallon(3.9))
@@ -37,4 +38,5 @@ print(liters_100km_to_miles_gallon(10.))
 print(miles_gallon_to_liters_100km(60.3))
 print(miles_gallon_to_liters_100km(31.4))
 print(miles_gallon_to_liters_100km(23.5))
+
   
