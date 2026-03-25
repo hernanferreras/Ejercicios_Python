72cb34a96e56a10af665cf368616dee09217ab4e
Hernan Ferreras <hernanferreras@icloud.com>
2026-03-25 07:38:57 -0300

    Add mysplit function to mimic split behavior
    Implement mysplit function to split a string into words based on whitespace.

---
+++ tu_propio_separador.py
@@ -0,0 +1,24 @@
+'''
+Ya conoces como funiona el método split(). Ahora queremos que lo pruebes.
+Tu tarea es escribir tu propia función, que se comporte casi como el método original split(), por ejemplo:
+
+*  Debe aceptar únicamente un argumento: una cadena.
+*  Debe devolver una lista de palabras creadas a partir de la cadena, dividida en los lugares donde la cadena contiene espacios en blanco.
+*  Si la cadena está vacía, la función debería devolver una lista vacía.
+*  Su nombre debe ser mysplit().
+
+Utiliza la plantilla en el editor. Prueba tu código con cuidado.
+'''
+
+def mysplit(strng):
+    #
+    # coloca tu código aquí
+    #
+
+
+print(mysplit("Ser o no ser, esa es la cuestión"))
+print(mysplit("Ser o no ser, esa es la cuestión"))
+print(mysplit("   "))
+print(mysplit(" abc "))
+print(mysplit(""))
+    
