2c8aa9cd80d7932ecdeb0dc607b1772d800d499c
Hernan Ferreras <hernanferreras@icloud.com>
2026-03-29 19:20:48 -0300

    Create Un_Display_LED.py for seven-segment simulation
    Implement a program to simulate a seven-segment display using individual LEDs for digits.

---
+++ Un_Display_LED.py
@@ -0,0 +1,16 @@
+'''
+Seguramente has visto un display de siete segmentos.
+Es un dispositivo (a veces electrónico, a veces mecánico) diseñado para presentar un dígito decimal utilizando un subconjunto de siete segmentos. 
+Tu tarea es escribir un programa que puede simular el funcionamiento de un display de siete segmentos, aunque vas a usar LEDs individuales en lugar de segmentos.
+Cada dígito es construido con 13 LEDs (algunos iluminados, otros apagados, por supuesto), así es como lo imaginamos:
+
+  # ### ### # # ### ### ### ### ### ###
+  #   #   # # # #   #     # # # # # # # 
+  # ### ### ### ### ###   # ### ### # # 
+  # #     #   #   # # #   # # #   # # # 
+  # ### ###   # ### ###   # ### ### ###
+Nota: el número 8 muestra todas las luces LED encendidas.
+
+Tu código debe mostrar cualquier número entero no negativo ingresado por el usuario.
+Consejo: puede ser muy útil usar una lista que contenga patrones de los diez dígitos.
+'''
