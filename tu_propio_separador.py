f7a7093847d3af082ba5f2d66ee8275feb42cb5e
Hernan Ferreras <hernanferreras@icloud.com>
2026-03-29 18:31:26 -0300

    Rename mysplit2 to mysplit and update logic

--- tu_propio_separador.py
+++ tu_propio_separador.py
@@ -9,44 +9,38 @@ Tu tarea es escribir tu propia función, que se comporte casi como el método or
 
 Utiliza la plantilla en el editor. Prueba tu código con cuidado.
 '''
-
-def mysplit2(a):
+def mysplit(a):
     b = []
     c = []
     x = ""
     solo_espacios = False
-    if len(a) == 0 or a[0] == chr(32):
+    if len(a) == 0:
         if len(a) == 0:
             return b
         else:
             for ch in a:
                 if ch == chr(32):
                     solo_espacios = True
-                else:
-                    solo_espacios = False
-                    break
         if solo_espacios == True:
-            return b
+            return c
     else:
         for i in range(len(a)):
-            if (a[i]== chr(32)):
+            if (a[i] == chr(32)):
                 b.append(x)
                 x = ""
             else:
                 x += a[i]
         b.append(x)
         for z in b:
-            print(z)    
             if z != '':
                 c.append(z)
     del b        
     return c
 
-
 cadena = input("Ingrese una cadena: ")
-print(mysplit2(cadena))
-print(mysplit2("Ser o no ser, esa es la cuestión"))
-print(mysplit2("Ser o no ser, esa es la cuestión"))
+print(mysplit(cadena))
+print(mysplit("Ser o no ser, esa es la cuestión"))
+print(mysplit("Ser o no ser, esa es la cuestión"))
 print("Esta cadena es solo espacios: ", mysplit2("   "))
-print("Esta cadena tiene espacio al final y principio: ", mysplit2(" abc "))
-print("Esta cadena esta vacia: ", mysplit2(""))
+print("Esta cadena tiene espacio al final y principio: ", mysplit(" abc "))
+print("Esta cadena esta vacia: ", mysplit(""))
