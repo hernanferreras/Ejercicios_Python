4c3886a62e3248fedd4e36c117626ba9cd8489e6
Hernan Ferreras <hernanferreras@icloud.com>
2026-03-29 18:41:30 -0300

    Refactor mysplit function for improved readability

--- tu_propio_separador.py
+++ tu_propio_separador.py
@@ -15,25 +15,26 @@ def mysplit(a):
     x = ""
     solo_espacios = False
     if len(a) == 0:
-        if len(a) == 0:
-            return b
-        else:
-            for ch in a:
-                if ch == chr(32):
-                    solo_espacios = True
+        return b
+    else:
+        for ch in a:
+            if ch == chr(32):
+                solo_espacios = True
+            else:
+                solo_espacios = False
+                break
         if solo_espacios == True:
             return c
-    else:
-        for i in range(len(a)):
-            if (a[i] == chr(32)):
-                b.append(x)
-                x = ""
-            else:
-                x += a[i]
-        b.append(x)
-        for z in b:
-            if z != '':
-                c.append(z)
+    for i in range(len(a)):
+        if (a[i] == chr(32)):
+            b.append(x)
+            x = ''
+        else:
+            x += a[i]
+    b.append(x)
+    for z in b:
+        if z != '':
+            c.append(z)
     del b        
     return c
 
@@ -41,6 +42,6 @@ cadena = input("Ingrese una cadena: ")
 print(mysplit(cadena))
 print(mysplit("Ser o no ser, esa es la cuestión"))
 print(mysplit("Ser o no ser, esa es la cuestión"))
-print("Esta cadena es solo espacios: ", mysplit2("   "))
+print("Esta cadena es solo espacios: ", mysplit("   "))
 print("Esta cadena tiene espacio al final y principio: ", mysplit(" abc "))
 print("Esta cadena esta vacia: ", mysplit(""))
